f37aa8deed4e5ad7dd95f83e709df19ea328ad74
Tio Saputra <karya.tiosaputra@gmail.com>
2018-04-21 05:25:15 +0700

    Input Post, Create A Comment, Validating Forms

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,4 +13,17 @@
     {{ $post->body }}
 
   </div><!-- /.blog-post -->
+
+  <div class="comments">
+      <ul class="list-group">
+      @foreach($post->comments as $comment)
+            <li class="list-group-item">
+                <strong>
+                    {{ $comment->created_at->diffForHumans() }}
+                </strong>
+                {{ $comment->body }}
+            </li>
+      @endforeach
+    </ul>
+  </div>
 @endsection
